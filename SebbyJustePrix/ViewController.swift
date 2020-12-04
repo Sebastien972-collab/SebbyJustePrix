318ec19cb0734d3646c1ef1705a51cf2146f2652
DAGUIN Sébastien <sebastien.daguin972@gmail.com>
2020-12-04 17:28:06 +0100

    First version

--- SebbyJustePrix/ViewController.swift
+++ SebbyJustePrix/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    let price = Int.random(in: 0...500)
+    
+    func justePrix(priceChoice : Int) {
+        print("ok")
+        if priceChoice > price {
+            displayPrice.text = "C'est moins"
+        }
+        else if priceChoice < price {
+            displayPrice.text = "C'est plus"
+        }
+        else {
+            displayPrice.text = "Vous avez gagner"
+        }
+    }
+    
+    @IBOutlet weak var displayPrice: UILabel!
+    
+    @IBOutlet weak var uPrice: UITextField!
+    
+    @IBAction func uValidatePrice(_ sender: Any) {
+        print(price)
+        let priceUser = Int(uPrice.text ?? "") ?? 0
+        justePrix(priceChoice: priceUser)
+        uPrice.text = ""
+    }
+    /*  if let uText =  Int(priceUser.text!){
+        justePrix(let: uText)
+        }else{
+            print("Ca ne fonctionne pas ma chabine")
+        }
+        */
+       
+        
+    
 }
 
